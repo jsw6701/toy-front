0e1d12c9bfa4ca172d9aad061aece504e34a8bdc
Glenn <anes53027@gmail.com>
2025-05-06 02:37:51 +0900

    fix: Method 수정

--- src/services/postService.ts
+++ src/services/postService.ts
@@ -42,7 +42,7 @@ export const fetchPostDetail = async (postId: number): Promise<Post> => {
     const response = await fetch(
       "http://localhost:8080/api/v1/post/detail",
       {
-        method: "POST",
+        method: "GET",
         headers: {
           "Content-Type": "application/json",
         },
